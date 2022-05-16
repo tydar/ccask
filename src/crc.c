63079a8a15cfc0b736b1c45f2721f2d3a1ff562b
Tyler Darnell <tyler.a.darnell@gmail.com>
2022-05-16 10:11:53 -0500

    add attribution for CRC32 code

--- src/crc.c
+++ src/crc.c
@@ -5,6 +5,10 @@
 
 uint32_t CRC_TABLE[256];
 
+/**@brief crc_init sets up the lookup table for the CRC computation
+ *
+ * Adapted from code fragment 8: https://en.wikipedia.org/wiki/Computation_of_cyclic_redundancy_checks#Generating_the_tables
+ * */
 void crc_init() {
 	uint32_t crc = 1;
 	CRC_TABLE[0] = 0;
@@ -21,6 +25,10 @@ void crc_init() {
 	}
 }
 
+/**@brief crc_print_table prints the computed CRC_TABLE to stdout for review.
+ *
+ * **Must not** be called before crc_init()
+ */
 void crc_print_table() {
 	for (size_t i = 0; i < 32; i++) {
 		for (size_t j = 0; j < 8; j++) {
@@ -29,6 +37,16 @@ void crc_print_table() {
 	}
 }
 
+/**@brief crc_compute computes the CRC32 result for given data
+ * 
+ * **MUST NOT** be called before crc_init()
+ *
+ * Adapted from https://en.wikipedia.org/wiki/Cyclic_redundancy_check#CRC-32_algorithm
+ *
+ * @param data an array of length data_length containing the data to compute the CRC value for
+ * @param data_length the size of the data array 
+ * @return the CRC value as a uint32_t
+ */
 uint32_t crc_compute(const uint8_t* data, size_t data_length) {
 	uint32_t crc32 = 0xFFFFFFFFu;
 
