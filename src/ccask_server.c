25993595b6f7a406fd7a8da748abdb5b6a93307a
Tyler Darnell <tyler.a.darnell@gmail.com>
2022-08-25 22:06:05 -0500

    Continue working to implement network socket interface for ccask. Done: get the socket & listen. Next: manage and poll connections & use ccask_query interface.

--- src/ccask_server.c
+++ src/ccask_server.c
@@ -1,3 +1,5 @@
+#define _POSIX_C_SOURCE 200112L
+
 #include <asm-generic/errno.h>
 #include <errno.h>
 #include <poll.h>
@@ -5,10 +7,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <sys/wait.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 
 #include "ccask_server.h"
 #include "util.h"
@@ -17,27 +23,103 @@
 #define MAX_CONN 5
 #define MAX_CMD_SIZE 1024 // 1 KB limit for now
 
-struct ccask_server {
-	unsigned int sd;
-	struct sockaddr_un local;
-};
+// TODO: finish work on my scratch file netw_sockets (~/scratch/netw_sockets)
+// and then move that implementation here
 
-ccask_server* ccask_server_init(ccask_server* srv, char* path) {
-	if (srv && path) {
-		srv->sd = socket(AF_UNIX, SOCK_STREAM, 0);
-		if (srv->sd == -1) {
-			perror("socket");
-			return 0;
+// helpers
+
+/*@brief get_in_addr ripped directly from beej's guide :+1:*/
+void *get_in_addr(struct sockaddr* sa) {
+	if (sa->sa_family == AF_INET) {
+		return &(((struct sockaddr_in*)sa)->sin_addr);
+	} 
+
+	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+unsigned int get_listener_socket(char* port) {
+	if (!port) return -1;
+	unsigned int listener;
+	int yes = 1;
+	int rv;
+
+	struct addrinfo hints, *ai, *p;
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+	if ((rv = getaddrinfo(NULL, port, &hints, &ai)) != 0) {
+		fprintf(stderr, "server: %s\n", gai_strerror(rv));
+		return -1;
+	}
+
+	for (p = ai; p != 0; p = p->ai_next) {
+		listener = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+		if (listener < 0) {
+			// error on socket()
+			perror("server: socket");
+			continue;
 		}
 
-		srv->local.sun_family = AF_UNIX;
-		strcpy(srv->local.sun_path, path);
-		unlink(srv->local.sun_path);
+		// drop "address already in use" message
+		if (setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+			// error on setsockopt()
+			perror("setsockopt");
+			continue;
+		}
+
+		if (bind(listener, p->ai_addr, p->ai_addrlen) == -1) {
+			// error on bind()
+			close(listener);
+			perror("server: bind");
+			continue;
+		}
+		
+		// if we made it here, we have successfully bound our listener socket
+		break;
+	}
+
+	// done with the addrinfo
+	freeaddrinfo(ai);
+
+	if (p == NULL) {
+		fprintf(stderr, "server: failed to bind\n");
+		return -1;
+	}
+
+	if (listen(listener, MAX_CONN) == -1) {
+		fprintf(stderr, "server: listen failed\n");
+		return -1;
+	}
+
+	return listener;
+}
+
+struct ccask_server {
+	int sd;	
+	unsigned int fd_count;
+	unsigned int fd_size;
+	struct pollfd *pfds;
+};
+
+ccask_server* ccask_server_init(ccask_server* srv, char* port) {
+	// 1) get our addr info using the passed port
+	// 2) bind our listening socket based on that
+	if (!port) port = "54569"; // a random default
+	if (srv && port) {
+		if((srv->sd = get_listener_socket(port)) == -1) {
+			*srv = (ccask_server){ 0 };
+			srv->sd = -1;
+			return srv;
+		};
+		srv->fd_count = 0;
+		srv->fd_size = 5;
+		srv->pfds = malloc(sizeof(*srv->pfds) * srv->fd_size);
 	} else {
 		*srv = (ccask_server){ 0 };
 		srv->sd = -1;
 	}
-
 	return srv;
 }
 
@@ -49,9 +131,8 @@ ccask_server* ccask_server_new(char* path) {
 
 void ccask_server_destroy(ccask_server* srv) {
 	if (srv) {
-		srv->sd = 0;
-		free(srv->local.sun_path);
-		srv->local = (struct sockaddr_un){ 0 };
+		free(srv->pfds);
+		*srv = (ccask_server){ 0 };
 	}
 }
 
@@ -61,18 +142,7 @@ void ccask_server_delete(ccask_server* srv) {
 }
 
 void ccask_server_print(ccask_server* srv) {
-	printf("sd: %d\npath: %s\n", srv->sd, srv->local.sun_path);
-}
-
-ccask_server* ccask_server_bind(ccask_server* srv) {
-	if (!srv) return 0;
-	size_t len = strlen(srv->local.sun_path) + sizeof(srv->local.sun_family);
-	if (bind(srv->sd, (struct sockaddr*)&(srv->local), len) == -1) {
-		perror("bind");
-		return 0;
-	}
-
-	return srv;
+	printf("sd: %d\nfd_count: %d\nfd_size: %d\n", srv->sd, srv->fd_count, srv->fd_size);
 }
 
 /**@brief recv_until reads len bytes into buf or returns -1 or -2 to indicate an error*/
@@ -124,8 +194,10 @@ int recv_cmd(int sockfd, uint8_t* buf, size_t bufsz, int flags) {
 	return len;
 }
 
+
+// TODO: re-write ccask_server_run to use in-struct
+// 		 pfds. Then wire up to ccask query interface from ccask_db
 int ccask_server_run(ccask_server* srv) {
-	srv = ccask_server_bind(srv);
 	if( !srv ) return 1;
 
 	struct sockaddr_un remote;
