c763b6a5bad4b37ba5255c4f6aaee4d1d72b576f
tydar <tyler.a.darnell@gmail.com>
2022-09-25 16:00:10 +0000

    Deploying to gh-pages from @ tydar/ccask@f600bd746e1a0aacc4923fa1891e0645ef06716d 🚀

--- menudata.js
+++ menudata.js
@@ -28,5 +28,13 @@ var menudata={children:[
 {text:"Files",url:"files.html",children:[
 {text:"File List",url:"files.html"},
 {text:"Globals",url:"globals.html",children:[
-{text:"All",url:"globals.html"},
-{text:"Functions",url:"globals_func.html"}]}]}]}
+{text:"All",url:"globals.html",children:[
+{text:"c",url:"globals.html#index_c"},
+{text:"h",url:"globals.html#index_h"},
+{text:"k",url:"globals.html#index_k"},
+{text:"s",url:"globals.html#index_s"}]},
+{text:"Functions",url:"globals_func.html",children:[
+{text:"c",url:"globals_func.html#index_c"},
+{text:"h",url:"globals_func.html#index_h"},
+{text:"k",url:"globals_func.html#index_k"},
+{text:"s",url:"globals_func.html#index_s"}]}]}]}]}
